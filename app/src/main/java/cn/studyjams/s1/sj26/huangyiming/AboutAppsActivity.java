93caf883816b11320e5420a781e9a3162a3a221d
navisac <yiming.whz@gmail.com>
2016-04-22 10:59:29 +0800

    mirror fix

--- app/src/main/java/cn/studyjams/s1/sj26/huangyiming/AboutAppsActivity.java
+++ app/src/main/java/cn/studyjams/s1/sj26/huangyiming/AboutAppsActivity.java
@@ -1,7 +1,10 @@
 package cn.studyjams.s1.sj26.huangyiming;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class AboutAppsActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class AboutAppsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_apps);
+    }
+
+    public void contactMe(View view) {
+        Intent contactMe = new Intent(Intent. ACTION_SENDTO);
+        contactMe.setData(Uri.parse("mailto:me@art2cat.com"));
+        if (contactMe.resolveActivity(getPackageManager()) != null)
+        {
+            startActivity(contactMe);
+        }
     }
 }
